647c56c87f376df60e54642eecb781b7e7b3c6e5
tomaszbielicki <tomek.bielicki@gmail.com>
2021-02-14 19:16:38 +0100

    init commit

---
+++ src/main/java/com/example/ProjectLekomat/ProjectLekomatApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectLekomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLekomatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLekomatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectLekomat/ProjectLekomatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectLekomat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLekomatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
